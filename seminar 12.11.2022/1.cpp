ac39ac3b305f8de08d335d7ff8afedecaf21d8cb
GenosseBlaackberry <51916553+GenosseBlaackberry@users.noreply.github.com>
2022-11-19 01:20:32 +0700

    Update 1.cpp

--- seminar 12.11.2022/1.cpp
+++ seminar 12.11.2022/1.cpp
@@ -6,22 +6,51 @@
 #include <deque>
 
 template <typename T>
-int Joseph(T begin, T end) {
-	auto pt = begin;
-	while (pt != end) {
-		std::cout << *pt << std::endl;
-		pt++;
+unsigned int Josephus(T container, unsigned int step, unsigned int index = 0) {
+	unsigned int size = container.size();
+
+	if (size == 1) {
+		return container[0];
 	}
-	return 0;
+	
+	index = (index + step) % size;
+
+	container.erase(container.begin() + index);
+
+	return Josephus(container, step, index);
+}
+
+void check() {
+	unsigned int const N = 1000;
+	unsigned int const STEP = 257;
+	clock_t clock();
+	CLOCKS_PER_SEC;
+
+	double start_t = (double)clock() / CLOCKS_PER_SEC;
+
+	std::cout << "vector" << std::endl;
+	std::vector <int> v;
+	for (unsigned int i = 0; i < N; i++) {
+		v.push_back(i);
+	}
+	std::cout << Josephus<std::vector <int>>(v, STEP) << std::endl;
+	double vector_t = (double)clock() / CLOCKS_PER_SEC;
+	std::cout << vector_t - start_t;
+
+	std::cout << std::endl;
+	std::cout << "deque" << std::endl;
+	std::deque <int> d;
+	for (unsigned int i = 0; i < N; i++) {
+		d.push_back(i);
+	}
+	std::cout << Josephus<std::deque <int>>(d, STEP) << std::endl;
+	double deque_t = (double)clock() / CLOCKS_PER_SEC;
+	std::cout << deque_t - vector_t;
+
 }
 
 
 int main()
 {
-	std::vector <int> v;
-	v.push_back(1);
-	v.push_back(3);
-	v.push_back(3);
-	v.push_back(7);
-	Joseph<std::vector <int> ::iterator>(v.begin(), v.end());
+	check();
 }
